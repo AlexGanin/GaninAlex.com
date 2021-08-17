89d2dabb73fe9ce34897569c8662695e7af0fa12
AlexGanin <711seller@gmail.com>
2021-08-17 22:04:57 +0300

    17.08.2021

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react"
 import Routes from "routes"
 import NavTop from "components/navTop"
 import { BrowserRouter } from "react-router-dom"
+//import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { AlertState } from "context/alert/AlertState"
+import { GithubState } from "context/github/GithubState"
 
 function App() {
     return (
       <>
-        <BrowserRouter>
-          <NavTop />
-          <Routes>
-          </Routes>
-        </BrowserRouter>
+        <GithubState>
+          <AlertState>
+            <BrowserRouter>
+              <NavTop />
+              <Routes>
+              </Routes>
+            </BrowserRouter>
+          </AlertState>
+        </GithubState>
       </>
     )
   
